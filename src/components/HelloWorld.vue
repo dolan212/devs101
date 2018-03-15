a897d1e77dbba6b5c16ef1cddc0fc8856bc73c1a
Brendan <brendanbath@gmail.com>
2018-03-15 14:22:01 +0200

    Getting setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,15 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+  <v-container grid-list-md text-xs-center fill-height>
+      <v-layout row wrap fill-height>
+        <v-flex m3 xs0>
+        </v-flex>
+        <v-flex xs12 m6>
+          <v-btn my.auto large>Get Started</v-btn>
+        </v-flex>
+        <v-flex m3 xs0>
+        </v-flex>
       </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+    </v-container>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
