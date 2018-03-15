6dc3221a04c2986d04dced6c6b499c078435ba71
dolan212 <dylandraper212@gmail.com>
2018-03-15 21:49:42 +0200

    started cytoscape

---
+++ src/components/skilltree.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-container grid-list-md text-xs-center fill-height>
+      		<v-layout row wrap fill-height>
+			<div id="cy" ref="cy" style="width:100%; height:100%;"></div>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+import cytoscape from 'cytoscape'
+export default {
+	data() {
+		return {
+		}
+	},
+	mounted() {
+		var cy = cytoscape({
+			container: this.$refs.cy,
+			elements: [
+				{
+					data: { id: 'a' }
+				},
+				{
+					data: { id: 'b' }
+				},
+				{
+					data: { id: 'ab', source: 'a', target: 'b' }
+				}
+
+			],
+			style: [
+				{
+					selector: 'node',
+					style: {
+						'background-color': '#666',
+						'label': 'data(id)'
+					}
+				}
+			],
+		});
+	}
+}
+</script>
