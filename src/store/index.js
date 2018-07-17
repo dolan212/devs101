669e209dec6993c65149ae6c192379454a36232d
dolan212 <u16081758@tuks.co.za>
2018-07-17 12:43:46 +0200

    Changed auto layout algorithm

--- src/store/index.js
+++ src/store/index.js
@@ -205,12 +205,14 @@ export const store = new Vuex.Store({
 
         autoLayout(state) {
             state.cy.layout({
-                name: 'circle',
+                name: 'cose',
 
                 fit: true, // whether to fit the viewport to the graph
                 padding: 30, // the padding on fit
                 boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
                 avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
+		ready: function() {},
+		nodeRepulsion: function(node) { return 2048; },
                 nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
                 spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
                 radius: undefined, // the radius of the circle
