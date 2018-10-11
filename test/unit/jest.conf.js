93610f9b7818855623006d72837e5d205fb78352
Natasha Draper <natasha@draper.net.za>
2018-10-11 13:42:00 +0200

    fixed testing

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "jest-transform-stub"
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup', 'jest-localstorage-mock'],
