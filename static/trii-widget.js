6290a6d5f7db65f93be48641f8c86f13a5d64d10
Natasha Draper <natasha@draper.net.za>
2018-10-11 17:25:28 +0200

    changed function parameters, and moved everything out of the global namespace

--- static/trii-widget.js
+++ static/trii-widget.js
@@ -1,6 +1,247 @@
-var widget_data = {};
+function trii_init(div_id, tree_json, callback, editable, globals) {
+    var widget_data = {};
+    var public_data = {};
+    /* 
+     * BEGIN FUNCTION DEFINITIONS
+     * These functions are defined within the trii_init method in order to avoid polluting the public namespace
+     * In order for a function to be externally accessible, it must be added to the public_data object, and used
+     * with the object passed to the callback
+     */
+    function refresh_globals() {
+        widget_data.global_div.innerHTML = '<h3>Global Variables</h3>';
+        if(widget_data.globals !== undefined) {
+            Object.keys(widget_data.globals).forEach(function(key, index) {
+                var p = document.createElement('p');
+                p.innerHTML = `<b>${key}:</b> ${widget_data.globals[key]}`;
+                widget_data.global_div.appendChild(p);
+            });
+        }
+    }
+    function node_select_handler(evt) {
+        widget_data.cy.nodes().not(evt.target).unselect();
+        var node = findNode(evt.target.id());
+        displayNodeDetails(node);
+    }
 
-function trii_init(div_id, tree_json, globals) {
+    function node_deselect_handler(evt) {
+        while(widget_data.info_div.firstChild)
+            widget_data.info_div.removeChild(widget_data.info_div.firstChild);
+    }
+
+    function findNode(id) {
+        var node = null;
+        widget_data.tree.nodes.forEach(function(item) {
+            if(item._id == id) {
+                node = item;
+            }
+        });
+        return node;
+    }
+
+    function loadJSON(json, callback) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', json, true);
+        xobj.onreadystatechange = function() {
+            if(xobj.readyState == 4 && xobj.status == "200") {
+                callback(JSON.parse(xobj.responseText));
+            }
+        };
+        xobj.send(null);
+    }
+
+    function displayNodeDetails(node) {
+        var name = document.createElement("p");
+        name.innerHTML = `<b>Skill:</b> ${node._label}`;
+        var desc = document.createElement("p");
+        desc.innerHTML = `<b>Description:</b> ${node._description}`;
+        widget_data.info_div.appendChild(name);
+        widget_data.info_div.appendChild(desc);
+        var skillpoints;
+        node.rules.forEach(function(item) {
+            if(item.type == "skillpoint") {
+                if(!skillpoints) skillpoints = document.createElement("p");
+                skillpoints.innerHTML = `<b>Skillpoints:</b> ${item.skillpoints}`;
+                node.skillpoints = parseInt(item.skillpoints);
+            }
+        });
+        if(skillpoints) widget_data.info_div.appendChild(skillpoints);
+        var buy_div = document.createElement("div");
+        buy_div.style.width = "100%";
+        buy_div.style.position = "relative";
+        buy_div.style.bottom = 0;
+        buy_div.style.display = "flex";
+        var times_bought = document.createElement("span");
+        times_bought.style.width = "60%";
+        times_bought.style.textAlign = "center";
+        
+        buy_div.style.position = "absolute";
+        buy_div.style.bottom = 0;
+        var num_bought = 0;
+        if(widget_data.times_bought) {
+            if(widget_data.times_bought[node._id])
+                num_bought = widget_data.times_bought[node._id];
+        }
+        if(editable) {
+            var minus_button = document.createElement("button");
+            minus_button.innerHTML = "-";
+            minus_button.style.width = "20%";
+            minus_button.style.color = "red";
+            var plus_button = document.createElement("button");
+            plus_button.innerHTML = "+";
+            plus_button.style.width = "20%";
+            plus_button.style.color = "green";
+            buy_div.appendChild(minus_button);
+            buy_div.appendChild(times_bought);
+            buy_div.appendChild(plus_button);
+        }
+        else {
+            times_bought.style.width = "100%";
+            buy_div.appendChild(times_bought);
+        }
+        
+        widget_data.info_div.appendChild(buy_div);
+        var error = document.createElement("span");
+        error.style.color = "red";
+        widget_data.info_div.appendChild(error);
+
+        function refresh() {
+            if(editable) {
+                if(num_bought >= node.max_times) plus_button.disabled = true;
+                else plus_button.disabled = false;
+
+                if(num_bought <= 0) minus_button.disabled = true;
+                else minus_button.disabled = false;
+            }
+            times_bought.innerHTML = `${num_bought}/${node.max_times}`;
+            error.innerHTML = '';
+            refresh_label(node);
+            refresh_globals();
+            refresh_edges(node);
+        }
+        refresh();
+
+        if(editable) {
+            plus_button.onclick = function(evt) {
+                try {
+                    num_bought = buy_skill(node);
+                    refresh();
+                } catch(exception) {
+                    error.innerHTML = exception;
+                }
+            }
+
+            minus_button.onclick = function(evt) {
+                try {
+                    num_bought = sell_skill(node);
+                    refresh();
+                } catch(exception) {
+                    error.innerHTML = exception;
+                }
+            }
+        }
+    }
+
+    function buy_skill(node) {
+        if(!editable) throw "Tree is not editable";
+        var dep = check_dependencies(node);
+        if(!dep) throw "You have not yet bought all the dependencies";
+        //check skillpoints
+        var sp = widget_data.globals['skillpoints'];
+        if(sp < node.skillpoints) throw "You do not have enough skillpoints";
+        if(widget_data.times_bought) {
+            if(widget_data.times_bought[node._id]) {
+                if(widget_data.times_bought[node._id] >= node.max_times)
+                    throw "You have reached the maximum amount of times you can buy this skill";
+                else  {
+                    widget_data.times_bought[node._id]++;
+                    widget_data.globals['skillpoints'] -= node.skillpoints;
+                }
+            }
+            else {
+                widget_data.times_bought[node._id] = 1;
+                widget_data.globals['skillpoints'] -= node.skillpoints;
+            }
+        } else {
+            widget_data.times_bought = [];
+            widget_data.times_bought[node._id] = 1;
+            widget_data.globals['skillpoints'] -= node.skillpoints;
+        }
+        return widget_data.times_bought[node._id];
+    }
+
+    function sell_skill(node) {
+        if(!editable) throw "Tree is not editable";
+        if(!widget_data.times_bought) throw "You have not purchased this skill";
+        if(!widget_data.times_bought[node._id] || widget_data.times_bought[node._id] <= 0) throw "You have not purchased this skill";
+        widget_data.times_bought[node._id]--;
+        widget_data.globals['skillpoints'] += node.skillpoints;
+        return widget_data.times_bought[node._id];
+    }
+
+    function check_dependencies(node) {
+        var valid = true;
+        node.rules.forEach(function(item) {
+            if(item.type == 'dependency') {
+                if(!widget_data.times_bought || !widget_data.times_bought[item.node] || widget_data.times_bought[item.node] <= 0) valid = false;
+            }
+        });
+        return valid;
+    }
+
+    function get_times_bought() {
+        return widget_data.times_bought;
+    }
+
+    function refresh_label(node) {
+        var cy_node = widget_data.cy.$(`#${node._id}`);
+        var times_bought = widget_data.times_bought[node._id] ? widget_data.times_bought[node._id] : 0;
+        cy_node.data('label', `${node._label} (${times_bought}/${node.max_times})`);
+    }
+    function refresh_edges(node) {
+            if(widget_data.times_bought[node._id] && widget_data.times_bought[node._id] > 0) {
+                widget_data.tree.edges.forEach(function(edge) {
+                    var cy_edge = widget_data.cy.$(`#${edge._id}`);
+                    if(edge._source == node._id) {
+                        cy_edge.style('line-color', 'green');
+                        cy_edge.style('target-arrow-color', 'green');
+                    }
+                })
+            }
+            else {
+                widget_data.tree.edges.forEach(function(edge) {
+                    var cy_edge = widget_data.cy.$(`#${edge._id}`);
+                    if(edge._source == node._id) {
+                        cy_edge.style('line-color', '#ccc');
+                        cy_edge.style('target-arrow-color', '#999');
+                    }
+                })
+            }
+    }
+
+    function set_times_bought(id, times_bought) {
+        var node = findNode(id);
+        var dep = check_dependencies(node);
+        if(!dep) {
+            var error = 'Error: prerequisite nodes not bought';
+            console.log(error);
+            throw error;
+        }
+        if(times_bought <= node.max_times) {
+            widget_data.times_bought[id] = times_bought;
+            refresh_label(node);
+            refresh_edges(node);
+        }
+        else {
+            var error = `Error: ${times_bought} is greater than the maximum of ${node.max_times} the node is allowed to be bought.`;
+            console.log(error);
+            throw error;
+        }
+    }
+
+    /*
+     * END FUNCTION DEFINITIONS
+     */
     var div = document.getElementById(div_id);
     div.style.display = "flex";
     div.style.border = "1px solid black";
@@ -11,8 +252,12 @@ function trii_init(div_id, tree_json, globals) {
     cy_div.style.display = "inline-block";
     cy_div.style.margin = 0;
     info_div.style.display = "block";
+    info_div.style.position = "absolute";
+    info_div.style.bottom = 0;
     info_div.style.margin = 0;
     global_div.style.display = "block";
+    global_div.style.position = "absolute";
+    global_div.style.top = 0;
     global_div.style.margin = 0;
 
     cy_div.style.width = "70%";
@@ -23,12 +268,12 @@ function trii_init(div_id, tree_json, globals) {
 
     right_hand_div.style.width = "30%";
     right_hand_div.style.height = "100%";
+    right_hand_div.style.position = "relative";
 
     global_div.style.width = "100%";
     global_div.style.height = "50%";
     //global_div.style.padding = "5px";
     global_div.style.borderBottom = "1px solid black";
-    global_div.innerHTML = '<h3>Global Variables</h3>';
 
     info_div.style.width = "100%";
     info_div.style.height = "50%";
@@ -40,6 +285,7 @@ function trii_init(div_id, tree_json, globals) {
     div.appendChild(cy_div);
     div.appendChild(right_hand_div);
     widget_data.info_div = info_div;
+    widget_data.global_div = global_div;
 
     widget_data.cy = cytoscape({
         container: cy_div
@@ -54,175 +300,26 @@ function trii_init(div_id, tree_json, globals) {
             });
             widget_data.tree = json.tree;
             widget_data.globals = json.globals;
-        });
-    }
-    if(globals) {
-        for(var glob in globals) {
-            if(globals.hasOwnProperty(glob)) {
-                if(widget_data.globals[glob]) {
-                    widget_data.globals[glob] = globals[glob];
+            widget_data.times_bought = [];
+            widget_data.cy.on('select', node_select_handler);
+            widget_data.cy.on('unselect', node_deselect_handler);
+            widget_data.tree.nodes.forEach(function(item) {
+                refresh_label(item);
+            });
+            if(globals) {
+                for(var glob in globals) {
+                    if(globals.hasOwnProperty(glob)) {
+                        widget_data.globals[glob] = globals[glob];
+                    }
                 }
             }
-        }
-    }
-
-    if(widget_data.globals) {
-        Object.keys(widget_data.globals).forEach(function(key, index) {
-            var p = document.createElement('p');
-            p.innerHTML = `<b>${key}:</b> ${widget_data.globals[key]}`;
-            global_div.appendChild(p);
+            refresh_globals();
+            public_data.set_times_bought = set_times_bought;
+            if(callback) callback(public_data);
         });
     }
-    widget_data.times_bought;
-
-    widget_data.cy.on('select', node_select_handler);
-    widget_data.cy.on('unselect', node_deselect_handler);
-        
-}
-
-function node_select_handler(evt) {
-    widget_data.cy.nodes().not(evt.target).unselect();
-    var node = findNode(evt.target.id());
-    displayNodeDetails(node);
-}
-
-function node_deselect_handler(evt) {
-    while(widget_data.info_div.firstChild)
-        widget_data.info_div.removeChild(widget_data.info_div.firstChild);
-}
-
-function findNode(id) {
-    var node = null;
-    widget_data.tree.nodes.forEach(function(item) {
-        if(item._id == id) {
-            node = item;
-        }
-    });
-    return node;
-}
-
-function loadJSON(json, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', json, true);
-    xobj.onreadystatechange = function() {
-        if(xobj.readyState == 4 && xobj.status == "200") {
-            callback(JSON.parse(xobj.responseText));
-        }
-    };
-    xobj.send(null);
-}
-
-function displayNodeDetails(node) {
-    var name = document.createElement("p");
-    name.innerHTML = `<b>Skill:</b> ${node._label}`;
-    var desc = document.createElement("p");
-    desc.innerHTML = `<b>Description:</b> ${node._description}`;
-    widget_data.info_div.appendChild(name);
-    widget_data.info_div.appendChild(desc);
-    var skillpoints;
-    node.rules.forEach(function(item) {
-        if(item.type == "skillpoint") {
-            if(!skillpoints) skillpoints = document.createElement("p");
-            skillpoints.innerHTML = `<b>Skillpoints:</b> ${item.skillpoints}`;
-        }
-    });
-    if(skillpoints) widget_data.info_div.appendChild(skillpoints);
-    var buy_div = document.createElement("div");
-    buy_div.style.width = "100%";
-    buy_div.style.position = "relative";
-    buy_div.style.bottom = 0;
-    buy_div.style.display = "flex";
-    var minus_button = document.createElement("button");
-    minus_button.innerHTML = "-";
-    minus_button.style.width = "20%";
-    var plus_button = document.createElement("button");
-    plus_button.innerHTML = "+";
-    plus_button.style.width = "20%";
-    var times_bought = document.createElement("span");
-    times_bought.style.width = "60%";
-    times_bought.style.textAlign = "center";
-    var num_bought = 0;
-    if(widget_data.times_bought) {
-        if(widget_data.times_bought[node._id])
-            num_bought = widget_data.times_bought[node._id];
-    }
-    
-    buy_div.appendChild(minus_button);
-    buy_div.appendChild(times_bought);
-    buy_div.appendChild(plus_button);
-    widget_data.info_div.appendChild(buy_div);
-    var error = document.createElement("span");
-    error.style.color = "red";
-    widget_data.info_div.appendChild(error);
-
-    function refresh() {
-        if(num_bought >= node.max_times) plus_button.disabled = true;
-        else plus_button.disabled = false;
-
-        if(num_bought <= 0) minus_button.disabled = true;
-        else minus_button.disabled = false;
-        times_bought.innerHTML = `${num_bought}/${node.max_times}`;
-        error.innerHTML = '';
-    }
-    refresh();
-
-    plus_button.onclick = function(evt) {
-        try {
-            num_bought = buy_skill(node);
-            refresh();
-        } catch(exception) {
-            error.innerHTML = exception;
-        }
-    }
-
-    minus_button.onclick = function(evt) {
-        try {
-            num_bought = sell_skill(node);
-            refresh();
-        } catch(exception) {
-            error.innerHTML = exception;
-        }
+    else {
+        throw "Error, please provide a JSON file for the tree";
     }
 }
 
-function buy_skill(node) {
-    var dep = check_dependencies(node);
-    if(!dep) throw "You have not yet bought all the dependencies";
-    if(widget_data.times_bought) {
-        if(widget_data.times_bought[node._id]) {
-            if(widget_data.times_bought[node._id] >= node.max_times)
-                throw "You have reached the maximum amount of times you can buy this skill";
-            else widget_data.times_bought[node._id]++;
-        }
-        else widget_data.times_bought[node._id] = 1;
-    } else {
-        widget_data.times_bought = [];
-        widget_data.times_bought[node._id] = 1;
-    }
-    return widget_data.times_bought[node._id];
-}
-
-function sell_skill(node) {
-    if(!widget_data.times_bought) throw "You have not purchased this skill";
-    if(!widget_data.times_bought[node._id] || widget_data.times_bought[node._id] <= 0) throw "You have not purchased this skill";
-    widget_data.times_bought[node._id]--;
-    return widget_data.times_bought[node._id];
-}
-
-function check_dependencies(node) {
-    var valid = true;
-    node.rules.forEach(function(item) {
-        if(item.type == 'dependency') {
-            if(!widget_data.times_bought || !widget_data.times_bought[item.node] || widget_data.times_bought[item.node] <= 0) valid = false;
-        }
-    });
-    return valid;
-}
-
-function get_times_bought() {
-    return widget_data.times_bought;
-}
-function set_times_bought(times_bought) {
-    widget_data.times_bought = times_bought;
-}
