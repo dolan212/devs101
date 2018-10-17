b18248430938b9c383e6d3e6f7aa04a149f9f827
Dail-Jonker <u13325630@tuks.co.za>
2018-10-18 01:01:14 +0200

    Logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 <v-container grid-list-md text-xs-center fill-height>
     <v-layout row wrap fill-height>
         <v-flex m2 xs0>
-            <v-img :src="logo" alt="Trii Logo" contain aspect-ratio="2.8">
+            <v-img :src="logo" alt="Trii Logo" contain aspect-ratio="4.4">
                 <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
                     <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
                 </v-layout>
